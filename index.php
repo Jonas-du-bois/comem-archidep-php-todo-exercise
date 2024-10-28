7ff29dd3cefd033d9370b802a07eba4f8eff9d79
Jonas Du Bois <jonas.du.bois@outlook.com>
2024-10-28 15:24:09 +0100

    update password

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ define('BASE_URL', '/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
-define('DB_PASS', 'ComemHeigVDArchi');
+define('DB_PASS', 'Economist1-Residency0-Nemesis0-Onlooker0');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
